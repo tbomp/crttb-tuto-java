f7c8a81c10e20a6020d82296eba9bf0855d4a9cf
Julien Aubert <j.aubert@actongroup.com>
2024-03-28 10:34:55 +0100

    demo

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #bin/sh
+
+## Tu as trouvé, explique pourquoi ça ne peut pas fonctionner pas besoin de me corriger
 sed -i 's/localhost/postgres-service/g' application.yml && java -jar app-java-forge-demo.jar
